4ca4be18175faa596296d8c666baa08a93d199d2
Aaron Lin <aaronlin256@gmail.com>
2022-08-16 18:03:16 -0400

    Show class and skills associated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+import { classes } from '../../constants/classes';
+import { skills } from '../../constants/skills';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +10,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  classes = classes;
+  testForm = new FormGroup({
+    testValue: new FormControl(`orange`),
+  });
+  tag = 'hello';
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
+  }
+
+  public getClassAvatar(className: string): any {
+    if (className in classes) {
+      return new URL(`../../assets/images/classes/${className}.png`, import.meta.url).href;
+    }
+
+    return new URL(`../../assets/images/classes/Warrior.png`, import.meta.url)
+      .href;
+  }
+
+  public getSkills(className: string): any[] {
+    return skills.filter(skill => skill.class === className);
   }
 
 }
