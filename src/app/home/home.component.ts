f0bf7aac826daad57f80016c3f2facdff29113aa
Aaron Lin <aaronlin256@gmail.com>
2022-08-16 21:10:39 -0400

    Display skills shown

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,15 +24,46 @@ export class HomeComponent implements OnInit {
 
   public getClassAvatar(className: string): any {
     if (className in classes) {
-      return new URL(`../../assets/images/classes/${className}.png`, import.meta.url).href;
+      return `./assets/images/classes/${className}.png`
     }
-
-    return new URL(`../../assets/images/classes/Warrior.png`, import.meta.url)
-      .href;
+    return`./assets/images/classes/Warrior.png`
   }
 
   public getSkills(className: string): any[] {
     return skills.filter(skill => skill.class === className);
+  }
+
+
+  public getSkillImage(id: number): any {
+    if (id > 99999) return this.getSkillImage(id / 10);
+    const s = this.getSkill(id);
+    if (id % 5 && !(s?.icon)) return this.getSkillImage(id - id % 5);
+
+    if (s != null && this.skillHasIcon(s)) {
+      return `assets/images/skills/${s.id}_${(s?.display ?? s.name).replace(":", "-")}.png`;
+  
+    }
+
+    return `assets/images/skills/unknown.png`;
+  }
+
+  public getSkillName(skill: any) {
+    const s = this.getSkill(skill.id);
+
+    if (s != null) {
+      if ('display' in s) return s.display;
+    }
+    return skill.name;
+  }
+
+  public skillHasIcon(s: any) {
+    if (s.name.startsWith("Basic Attack") || s?.display?.startsWith("Basic Attack") || !(s?.icon ?? true))
+      return false;
+    return true;
+  }
+
+  public getSkill(id: any) {
+    return skills.find((k) => k.id == id);
   }
 
 }
