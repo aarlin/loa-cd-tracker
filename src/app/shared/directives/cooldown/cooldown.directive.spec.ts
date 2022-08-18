6aa78a3b5b9d79155d4a10005aae90026131091b
Aaron Lin <aaronlin256@gmail.com>
2022-08-18 02:06:48 -0400

    Create cooldown directive

---
+++ src/app/shared/directives/cooldown/cooldown.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CooldownDirective } from './cooldown.directive';
+
+describe('CooldownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CooldownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
