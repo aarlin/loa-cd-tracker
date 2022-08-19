6bd2c497a3dc3a9e43b1e636530275d3a7703a0b
Aaron Lin <aaronlin256@gmail.com>
2022-08-19 02:05:57 -0400

    Add animate.css when cooldown is ready

--- src/app/shared/directives/cooldown/cooldown.directive.ts
+++ src/app/shared/directives/cooldown/cooldown.directive.ts
@@ -4,28 +4,62 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[cooldown]'
 })
 export class CooldownDirective {
+  SECOND_IN_MS = 1000;
+  UPDATE_INTERVAL = this.SECOND_IN_MS / 60; // Update 60 times per second (60 FPS)
+  SKILL_CLASS = 'skill';
+  DISABLED_CLASS = 'disabled';
+  
+  // Cooldowns per skill in milliseconds
+  COOLDOWN_MAP = new Map([
+    ['Moon Flash Kick', 22000],
+    ['jump', 2000],
+    ['crawl', 3000],
+    ['slide', 4000],
+    ['tumble', 5000],
+  ]);
+
+
   constructor(private el: ElementRef) {
     console.log(this.el.nativeElement.src);
   }
 
   @HostListener('click', ['$event'])
   onClick(e: any) {
-    console.log(e)
+    console.log(e);
+    this.activateSkill(e);
   }
 
-  @HostListener('mouseenter') onMouseEnter() {
-    this.onCooldown('');
-  }
-
-  @HostListener('mouseleave') onMouseLeave() {
-    this.isAvailable('');
-  }
-
-  private onCooldown(color: string) {
-    this.el.nativeElement.style.filter = "grayscale(100%)";
-  }
-
-  private isAvailable(color: string) {
-    this.el.nativeElement.style.filter = "initial";
+  private activateSkill = (event: any) => {
+    const {target} = event;
+    
+    // Exit if we click on anything that isn't a skill
+    if(!target.classList.contains(this.SKILL_CLASS)) return;
+    
+    target.classList.add(this.DISABLED_CLASS);
+    
+    // Get cooldown time
+    const skill = target.dataset.skill;
+    let time = this.COOLDOWN_MAP.get(skill) ?? 0 - this.UPDATE_INTERVAL;
+    
+    // Update remaining cooldown
+    const intervalID = setInterval(() => {
+      // Pass remaining time in percentage to CSS
+      const passedTime = time / (this.COOLDOWN_MAP.get(skill) ?? 0);
+      target.style.filter = `grayscale(${passedTime})`;
+      
+      // Display time left
+      target.textContent = (time / this.SECOND_IN_MS).toFixed(2);
+      time -= this.UPDATE_INTERVAL;
+      
+      // Stop timer when there is no time left
+      if(time < 0) {
+        target.textContent = '';
+        target.style = '';
+        target.classList.remove(this.DISABLED_CLASS);
+        target.classList.add("animate__animated", "animate__headShake");
+        
+        clearInterval(intervalID);
+      }
+    }, this.UPDATE_INTERVAL);
   }
 }
